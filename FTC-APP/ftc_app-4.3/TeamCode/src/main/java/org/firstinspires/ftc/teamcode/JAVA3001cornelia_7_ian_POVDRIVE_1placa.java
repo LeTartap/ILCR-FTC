079c55e5be2cd9a1db992595fd8c30817d2a5625
David Galati <david.galati01@yahoo.com>
2019-02-02 01:22:13 +0200

    tele gata inainte de demo

--- FTC-APP/ftc_app-4.3/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JAVA3001cornelia_7_ian_POVDRIVE_1placa.java
+++ FTC-APP/ftc_app-4.3/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JAVA3001cornelia_7_ian_POVDRIVE_1placa.java
@@ -4,16 +4,21 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
+
 
 @TeleOp(name = "JAVA3001cornelia_7_ian_POVDRIVE_1placa (Blocks to Java)", group = "")
 public class JAVA3001cornelia_7_ian_POVDRIVE_1placa extends LinearOpMode {
 
   private DcMotor stanga0;
   private DcMotor dreapta1;
-  private Servo s0;
   private DcMotor arm2;
   private DcMotor extension3;
+
+  CRServo s0;
+  CRServo s2;
+  CRServo s1;
+
 
   /**
    * This function is executed when this Op Mode is selected from the Driver Station.
@@ -22,7 +27,9 @@ public class JAVA3001cornelia_7_ian_POVDRIVE_1placa extends LinearOpMode {
   public void runOpMode() {
     stanga0 = hardwareMap.dcMotor.get("stanga0");
     dreapta1 = hardwareMap.dcMotor.get("dreapta1");
-    s0 = hardwareMap.servo.get("s0");
+    s0 = hardwareMap.crservo.get("s0");
+    s1=hardwareMap.crservo.get("s1");
+    s2=hardwareMap.crservo.get("s2");
     arm2 = hardwareMap.dcMotor.get("arm2");
     extension3 =hardwareMap.dcMotor.get("extension3");
 
@@ -31,6 +38,8 @@ public class JAVA3001cornelia_7_ian_POVDRIVE_1placa extends LinearOpMode {
     // In this example, the right motor was reversed so that positive
     // applied power makes it move the robot in the forward direction.
     stanga0.setDirection(DcMotorSimple.Direction.REVERSE);
+//    dreapta1.setDirection(DcMotorSimple.Direction.REVERSE);
+
     waitForStart();
     if (opModeIsActive()) {
       // Put run blocks here.
@@ -42,22 +51,17 @@ public class JAVA3001cornelia_7_ian_POVDRIVE_1placa extends LinearOpMode {
         // the topmost position corresponds to maximum forward power.
         stanga0.setPower(-gamepad1.left_stick_y + gamepad1.right_stick_x);
         dreapta1.setPower(-gamepad1.left_stick_y - gamepad1.right_stick_x);
-        while (gamepad1.right_bumper) {
-          s0.setPosition(1);
-        }
-        while (gamepad1.left_bumper) {
-          s0.setPosition(-1);
-        }
+
         while (gamepad1.dpad_up) {
           arm2.setPower(-0.5);
         }
         while (gamepad1.dpad_down) {
           arm2.setPower(0.5);
         }
-        while (gamepad1.x) {
+        while (gamepad1.left_trigger>0) {
           arm2.setPower(-1);
         }
-        while (gamepad1.y) {
+        while (gamepad1.right_trigger>0) {
           arm2.setPower(1);
         }
         while(gamepad1.dpad_left){
@@ -66,11 +70,32 @@ public class JAVA3001cornelia_7_ian_POVDRIVE_1placa extends LinearOpMode {
         while(gamepad1.dpad_right){
           extension3.setPower(-1);
         }
+          while (gamepad1.right_bumper) {
+              s0.setPower(1);
+          }
+          while (gamepad1.left_bumper) {
+              s0.setPower(-1);
+          }
+          while(gamepad1.a){
+              s1.setPower(-1);
+              s2.setPower(1);
+          }
+          while(gamepad1.y)
+          {
+              s1.setPower(1);
+              s2.setPower(-1);
+          }
+          while (gamepad1.left_stick_button) {
+              s0.setPower(0);
+          }
+          s1.setPower(0);
+          s2.setPower(0);
 
-        arm2.setPower(0);
-        extension3.setPower(0);
+          arm2.setPower(0);
+          extension3.setPower(0);
         arm2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        // s0.setPosition(0);
+        extension3.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
         telemetry.addData("Left Pow", stanga0.getPower());
         telemetry.addData("Right Pow", dreapta1.getPower());
         telemetry.update();
--- FTC-APP/ftc_app-4.3/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/VisionJavaExample.java
+++ FTC-APP/ftc_app-4.3/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/VisionJavaExample.java
@@ -4,6 +4,10 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.vuforia.Vuforia;
 
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
@@ -14,17 +18,31 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 public class VisionJavaExample extends LinearOpMode{
     MasterVision vision;
     SampleRandomizedPositions goldPosition;
+    private DcMotor stanga0;
+    private DcMotor dreapta1;
+    private CRServo s0;
+    private CRServo s1;
+    private CRServo s2;
+    private DcMotor arm2;
+    private DcMotor extension3;
 
     @Override
     public void runOpMode() throws InterruptedException {
+        stanga0 = hardwareMap.dcMotor.get("stanga0");
+        dreapta1 = hardwareMap.dcMotor.get("dreapta1");
+        s0 = hardwareMap.crservo.get("s0");
+        s0 = hardwareMap.crservo.get("s1");
+        s0 = hardwareMap.crservo.get("s2");
+        arm2 = hardwareMap.dcMotor.get("arm2");
+        extension3 =hardwareMap.dcMotor.get("extension3");
         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
         parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;// recommended camera direction
         parameters.vuforiaLicenseKey = "AWW9QST/////AAABmQteW8XAo0pNgaCx1lJdVKJZTDDuNOY0d6INToiFK3enrsIjMHnETEAjp+Xnr1G0cSZHi6XKqWxGccgCTVB2StwRTA1fxHS/IgfodmrCXBioTEnLzHjHZg1Pl3pFOhpF83DoY8Z7oP8kZHYFDAa5M2DFdqnyNGzTCo6uG8dYjD5Vb76KI7rhJwyCkOh22l8lBj4VL5wNIM1Jp3svMwafo44IFcg9Av3khzwhO+OMD8+/QPQ7Ifj9a/G4NuOTEXhFRiDBQ/9mX/cPOgkX6OARb1QwYbpM7bFLYkVUT6R3Tt5C2S6ANEuO9ciCDmDZYs94AKj3AIDDTNvexq1akGwLqwzeXvhHVx1Y3QknpxoLeNPC";
 
-        vision = new MasterVision(parameters, hardwareMap, true, MasterVision.TFLiteAlgorithm.INFER_NONE);
+        vision = new MasterVision(parameters, hardwareMap, false, MasterVision.TFLiteAlgorithm.INFER_NONE);
         vision.init();// enables the camera overlay. this will take a couple of seconds
         vision.enable();// enables the tracking algorithms. this might also take a little time
-
+        stanga0.setDirection(DcMotorSimple.Direction.REVERSE);
         waitForStart();
 
         vision.disable();// disables tracking algorithms. this will free up your phone's processing power for other jobs.
@@ -33,21 +51,52 @@ public class VisionJavaExample extends LinearOpMode{
 
         while(opModeIsActive()){
             telemetry.addData("goldPosition was", goldPosition);// giving feedback
-
+//            extension3.setPower(-1);
+//            sleep(3350);
+            extension3.setPower(0);
             switch (goldPosition){ // using for things in the autonomous program
                 case LEFT:
                     telemetry.addLine("going to the left");
+                    stanga0.setPower(-0.2);
+                    dreapta1.setPower(0.2);
+                    sleep(700);
+                    stanga0.setPower(0.5);
+                    dreapta1.setPower(0.5);
                     break;
                 case CENTER:
                     telemetry.addLine("going straight");
+                    stanga0.setPower(0.5);
+                    dreapta1.setPower(0.5);
+                    sleep(800);
+                    stanga0.setPower(0);
+                    dreapta1.setPower(0);
                     break;
                 case RIGHT:
                     telemetry.addLine("going to the right");
+                    stanga0.setPower(0.2);
+                    dreapta1.setPower(-0.2);
+                    sleep(700);
+                    stanga0.setPower(0.5);
+                    dreapta1.setPower(0.5);
+                    sleep(800);
+                    stanga0.setPower(0);
+                    dreapta1.setPower(0);
                     break;
                 case UNKNOWN:
                     telemetry.addLine("staying put");
+                    stanga0.setPower(0.2);
+                    dreapta1.setPower(-0.2);
+                    sleep(700);
+                    stanga0.setPower(0.5);
+                    dreapta1.setPower(0.5);
+                    sleep(700);
+                    stanga0.setPower(0);
+                    dreapta1.setPower(0);
+
                     break;
             }
+            stanga0.setPower(0);
+            dreapta1.setPower(0);
 
             telemetry.update();
         }
